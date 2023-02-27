6724522f47531c1b19f6815c0454406c907a3b3c
psnaz <psnazle@gmail.com>
2023-02-27 15:13:53 +0000

    Add checkout views and templates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
     # Other
     'storages',
+    'crispy_forms',
 ]
 
 
@@ -67,6 +68,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'boutique_ado.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -81,7 +84,12 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
+            ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ],
         },
     },
